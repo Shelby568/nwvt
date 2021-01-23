aba289a90c3af4bb90512675c3eb18f3a1d2b2c2
Shelby <shelby.harrison6@hotmail.com>
2021-01-23 13:20:15 +0000

    added multiple modal functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import '../styles/App.css';
 import Home from './Home';
-import { Switch, Route } from 'react-router-dom'
-import Contact from './Contact';
 
 function App() {
   return (
     <div className="App">
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/contactus" component={Contact} />
-      </Switch>
+      <Home />
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,55 +1,60 @@
-import React, { useState } from 'react';
+import React from 'react';
 import logo from '../images/logo.png'
 import background from '../images/background2.png'
 import '../styles/Home.css';
-import { Link } from 'react-router-dom';
-import Modal from 'react-modal';
 import InfoModal from '../components/InfoModal';
+import Contact from '../components/Contact';
+import { ModalContext, ModalProvider, ModalRoot } from 'react-multi-modal';
 
 const Home = () => {
-    const [ modalIsOpen, setIsOpen ] = useState(false);
 
-    const customStyles = {
-        content: {
-            height: '45%',
-            width: '45%',
-            top: '50%',
-            left: '50%',
-            right: 'auto',
-            bottom: 'auto',
-            marginRight: '-50%',
-            transform: 'translate(-50%, -50%)',
-            overflow: 'hidden',
-        },
-    };
+   function ModalOne({ isOpen, hideModal }) {
+        return (
+            isOpen && (
+                <InfoModal />
+            )
+        );
+   };
 
-    const openModal = () => {
-        setIsOpen(true);
-    };
+   function ModalTwo({ isOpen, hideModal }) {
+       return (
+           isOpen && (
+               <Contact />
+           )
+       );
+   };
 
-    const closeModal = () => {
-        setIsOpen(false);
-    };
+   function showModalOne(showModal) {
+       showModal({
+           component: ModalOne,
+           modalProps: customStyles,
+       });
+   };
+
+   function showModalTwo(showModal) {
+       showModal({
+           component: ModalTwo,
+           modalProps: customStyles,
+       });
+   };
 
     return (
     <div className="home">
         <img src={logo} className="logo-image" alt="logo" />
         <h2 className="logo-intro">Be confident knowing your vehicle is protected</h2>
-            <button className="but1" onClick={() => {openModal()}}>Let's talk trackers</button>
-            <div className="divider" />
-            <Link to="/contactus">
-            <button className="but2">Contact us</button>
-            </Link>
-            <img src={background} className="background-image" alt="car" />
-            <Modal
-                shouldCloseOnOverlayClick={true}
-                isOpen={modalIsOpen}
-                onRequestClose={closeModal}
-                style={customStyles}
-                ariaHideApp={false}
-            >
-                <InfoModal />
-            </Modal>
+<ModalProvider>
+    <ModalContext>
+    {({ showModal, hideModal }) => (
+    <>
+        <button className="but1" onClick={() => {showModalOne(showModal)}}>Let's talk trackers</button>
+    <div className="divider" />
+        <button className="but2" onClick={() => {showModalTwo(showModal)}}>Contact us</button>
+    <ModalRoot />
+    </>
+    )}
+    </ModalContext>
+</ModalProvider>
+        <img src={background} className="background-image" alt="car" />
     </div>
     )
 };
